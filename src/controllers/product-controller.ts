0ba813ac6204ec231ac63261044496dc9633ec64
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-09-06 15:33:26 -0300

    feat: create update products controller

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -32,6 +32,30 @@ class ProductController {
       next(error);
     }
   }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "Id must be a number." })
+        .parse(request.params.id);
+
+      const bodySchema = z.object({
+        name: z.string().trim().min(3),
+        price: z.number().gt(0, { message: "Price must be greater than 0" }),
+      });
+      const { name, price } = bodySchema.parse(request.body);
+
+      await knex<ProductRepository>("products")
+        .update({ name, price, updated_at: knex.fn.now() })
+        .where({ id });
+
+      return response.json();
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export { ProductController };
