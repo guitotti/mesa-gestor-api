d0dff2e591e89a9bf9538d3bd09ed5e63e39c2cb
unknown <dev.guilhermetotti@gmail.com>
2025-09-05 22:26:37 -0300

    feat: create product functionality

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -1,9 +1,27 @@
 import { NextFunction, Request, Response } from "express";
+import { knex } from "@/database/knex";
+import z from "zod";
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       return response.json({ message: "Ok" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(3),
+        price: z.number().gt(0, { message: "Price must be greater than 0" }),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+      await knex<ProductRepository>("products").insert({ name, price });
+
+      return response.status(201).json();
     } catch (error) {
       next(error);
     }
